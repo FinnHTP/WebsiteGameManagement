3f2af3a657c3838f36b988d9a7fb55919757fee7
PhatWataTek <justin.pham@watatek.com>
2024-06-19 23:13:50 +0700

    updated

--- src/main/java/com/project/backend/config/SecurityConfiguration.java
+++ src/main/java/com/project/backend/config/SecurityConfiguration.java
@@ -1,65 +1,62 @@
-package com.project.backend.config;
-
-import com.project.backend.utilies.RoleName;
-import jakarta.servlet.Filter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfiguration {
-    private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
-            "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
-            "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
-            "/api/test/**", "/authenticate" };
-
-
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-
-
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(WHITE_LIST_URL).permitAll()
-                        .requestMatchers(
-                        "/api/v1/auth/**",
-                                "/api/password/**"
-
-                                ).permitAll()
-
-//                        .requestMatchers("/api/**").hasAuthority(String.valueOf(RoleName.ADMIN))
-                        .anyRequest().authenticated())
-                .sessionManagement(session -> session // Sử dụng cấu hình mặc định cho session management
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-                        .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//                .oauth2Login(oauth2Login ->
-//                        oauth2Login
-//                                .defaultSuccessUrl("/oauth2", true)
-//                                .failureUrl("/login?error=true")
-//                );
-//        http.authorizeHttpRequests(authorize ->
-//                authorize .anyRequest()
-//                        .authenticated()
-//                        .and()
-//                        .oauth2Login()
-//                        .defaultSuccessUrl("user",true);
-//        });
-        return http.build();
-    }
-}
+//package com.project.backend.config;
+//
+//import com.project.backend.utilies.RoleName;
+//import jakarta.servlet.Filter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfiguration {
+//    private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
+//            "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+//            "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
+//            "/api/test/**", "/authenticate" };
+//
+//
+//    private final JwtAuthenticationFilter jwtAuthFilter;
+//    private final AuthenticationProvider authenticationProvider;
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//
+//                .authorizeHttpRequests(authorize -> authorize.requestMatchers(WHITE_LIST_URL).permitAll()
+//                        .requestMatchers(
+//                        "/api/v1/auth/**",
+//                                "/api/password/**"
+//
+//                                ).permitAll()
+//
+////                        .requestMatchers("/api/**").hasAuthority(String.valueOf(RoleName.ADMIN))
+//                        .anyRequest().authenticated())
+//                .sessionManagement(session -> session // Sử dụng cấu hình mặc định cho session management
+//                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+//                        .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+////                .oauth2Login(oauth2Login ->
+////                        oauth2Login
+////                                .defaultSuccessUrl("/oauth2", true)
+////                                .failureUrl("/login?error=true")
+////                );
+////        http.authorizeHttpRequests(authorize ->
+////                authorize .anyRequest()
+////                        .authenticated()
+////                        .and()
+////                        .oauth2Login()
+////                        .defaultSuccessUrl("user",true);
+////        });
+//        return http.build();
+//    }
+//}
